cbbeed82244a3770b43f4fb6901d75c229e1affe
Dérick Gagnon <derickgagnon@Dericks-MacBook-Pro.local>
2023-07-16 22:51:43 -0400

    offers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './components/Hero.jsx';
 import Navbar from './components/Navbar.jsx';
+import Offers from './components/Offers.jsx';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
     </div>
   );
 }

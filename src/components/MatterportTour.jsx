d42a028aecc9266b4c6ff0f6450d3d0c890aeb2e
derickGagnon <derick.gag46@hotmail.com>
2023-08-19 07:45:07 -0700

    materpost complet

--- src/components/MatterportTour.jsx
+++ src/components/MatterportTour.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 const MatterportTour = () => {
   return (
-    <div className="flex justify-center">
-      <h2 className="text-2xl">Explore Our 3D Virtual Tour</h2>
-      <div>
-        <div className="py-10">
+    <div className="max-w-[1400px] m-auto py-16 px-4 flex flex-col justify-center">
+      <h2 className="text-4xl text-center p-4">Explore Our 3D Virtual Tour</h2>
+        <div className="aspect-w-16 aspect-h-9">
           <iframe
-            width="720"
-            height="576"
+            title="matterpost video"
+            frameborder="0" 
+            allow="autoplay; fullscreen; picture-in-picture" 
+            allowfullscreen
             src="https://gallery.we-get-around.com/3d-model/250-pharr-road/fullscreen/?embedded"
-            allowfullscreen="allowfullscreen"
           />
         </div>
-      </div>
     </div>
   );
 };
